3290377f1e13e220b8a5a13d3d0d80f04601640b
Andrew Andrewsson <sidor@adamed.com>
2020-08-01 17:53:47 +0200

    additional tests for form

--- src/tests/components/TodoForm.test.js
+++ src/tests/components/TodoForm.test.js
@@ -2,6 +2,8 @@ import TodoForm from '../../components/TodoForm'
 import { shallow } from 'enzyme'
 import React from 'react'
 import mockTodos from '../fixtures/todosFixture'
+import moment from 'moment'
+import { SingleDatePicker } from 'react-dates'
 
 test('Should be able to render TodoForm component correctly', () => {
     const wrapper = shallow(<TodoForm/>)
@@ -9,7 +11,7 @@ test('Should be able to render TodoForm component correctly', () => {
 })
 
 test('Should render TodoForm with data correctly', () => {
-    const wrapper = shallow(<TodoForm expense={mockTodos[0]}/>)
+    const wrapper = shallow(<TodoForm todo={mockTodos[0]}/>)
     expect(wrapper).toMatchSnapshot()
 })
 
@@ -48,4 +50,32 @@ test('Should set priority on valid input change', () => {
         target: { value }
     })
     expect(wrapper.state('priority')).toBe(value)
+})
+
+test('Should call onSubmit prop on valid form submission', () => {
+    const onSubmitSpy = jest.fn()
+    const wrapper = shallow(<TodoForm todo={mockTodos[0]} onSubmit={onSubmitSpy} />)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    })
+    expect(wrapper.state('error')).toBe('')
+    expect(onSubmitSpy).toHaveBeenLastCalledWith({
+        title: mockTodos[0].title,
+        priority: mockTodos[0].priority,
+        createdAt: mockTodos[0].createdAt.valueOf(),
+        details: mockTodos[0].details
+    })
+})
+
+test('Should correctly set date', () => {
+    const newDate = moment()
+    const wrapper = shallow(<TodoForm />)
+    wrapper.find('SingleDatePicker').prop('onDateChange')(newDate)
+    expect(wrapper.state('createdAt')).toBe(newDate)
+})
+
+test('Should correctly set focus', () => {
+    const wrapper = shallow(<TodoForm />)
+    wrapper.find('SingleDatePicker').prop('onFocusChange')({focused: true})
+    expect(wrapper.state('calendarFocused')).toBe(true)
 })
