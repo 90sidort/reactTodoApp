7cac89f9929b82fe4d0b47e50a099cb1b22287f0
Andrew Andrewsson <sidor@adamed.com>
2020-08-14 18:07:43 +0200

    setup dist and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path')
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = (env) => {
   const isProduction = env === 'production'
-  const CSSExtract = new ExtractTextPlugin('style.css')
+  const CSSextract = new ExtractTextPlugin('styles.css')
 
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -18,30 +18,30 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: CSSExtract.extract({
+        use: CSSextract.extract({
           use: [
-            {
-              loader: 'css-loader',
+            {loader: 'css-loader',
               options: {
                 sourceMap: true
               }
             },
-            {
-              loader: 'sass-loader',
+            {loader: 'sass-loader',
               options: {
                 sourceMap: true
               }
             }
-        ]})
+          ]
+        })
       }]
     },
     plugins: [
-      CSSExtract
+      CSSextract
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   }
 }
