7e59c89ac9a2766e352264bdb19007ed03c17758
andrew <90sidort@gmail.com>
2020-09-17 20:48:37 +0200

    styling header & logn

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,39 @@ import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>Todo app</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active" exact={true}>
-      Create todo
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active" exact={true}>
-      Help
-    </NavLink>
-    <button onClick={startLogout}>Logout</button>
+  <header className="header">
+    <div className="headerContent">
+      <div className="headerMenu">
+        <h1 className="header__title">Todo app</h1>
+        <NavLink
+          to="/"
+          activeClassName="is-active"
+          exact={true}
+          className="linkElement"
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="/create"
+          activeClassName="is-active"
+          exact={true}
+          className="linkElement"
+        >
+          Create todo
+        </NavLink>
+        <NavLink
+          to="/help"
+          activeClassName="is-active"
+          exact={true}
+          className="linkElement"
+        >
+          Help
+        </NavLink>
+      </div>
+      <button onClick={startLogout} className="buttonLogout">
+        Logout
+      </button>
+    </div>
   </header>
 );
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,14 @@ import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div>
-      <button onClick={startLogin}>Login with Google</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Todo App</h1>
+        <p>It's time to get more organized!</p>
+        <button className="buttonBlue" onClick={startLogin}>
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 };
